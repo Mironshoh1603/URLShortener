11c32eaab966f4e9f62e1870efff0b0aae144827
mironshoh1603 <mironshohasadov1603@gmail.com>
2022-12-17 22:21:02 +0500

    created route for UrlsController

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UrlService } from './url.service';
 
-@Controller('url')
+@Controller()
 export class UrlController {
   constructor(private readonly urlService: UrlService) {}
+  @Get("/shrink/:url")
+  getShrink(@Param('url') url:string){
+    return this.urlService.create(url)
+  }
+  // @Get("/shrink")
+  // getShrinkStr():string{
+  //   return "sasdfgv"
+  // }
+
+  @Post(":shrink")
+  postShrink(@Body() body:{url:string}){
+    return this.urlService.create(body.url)
+  }  
+  @Get('/s/:shortenedUrl')
+  unshrink(@Param('shortenedUrl') shortenedUrl:string){
+    return this.urlService.find(shortenedUrl)
+  }
 }
